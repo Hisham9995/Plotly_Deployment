795df400980339ee79e91946886e17cccbbd7967
HISHAM ALMANSURI <82621077+Hisham9995@users.noreply.github.com>
2021-07-18 08:37:58 -0400

    plots.js

---
+++ plots.js
@@ -0,0 +1,37 @@
+function init() {
+  // Reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+//The list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
+
+init();
+// Initialize the dashboard
+
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+
+// Demographics Panel
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(result.location);
+  });
+}
